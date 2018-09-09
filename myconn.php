98eb8b7ea17d799a5ee16506ae4526f747eef9bc
TSchompoo <40933595+TSchompoo@users.noreply.github.com>
2018-09-09 17:49:31 +0700

    Add files via upload

---
+++ myconn.php
@@ -0,0 +1,17 @@
+<?php
+$servername = ":plearnja.com;
+$username = ":plearnja_cpsu7";
+$password = "lzXuQCgB";
+$dbname = ":plearnja_cpsu7";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Set Charset
+mysqli_set_charset($conn, "utf8");
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
